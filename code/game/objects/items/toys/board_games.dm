4645faeb962f2c0d2fd7d06edfbe8e35405a4fe3
Kryth <kendrickmallare.km@gmail.com>
2023-01-29 20:27:36 +0800

    update

--- code/game/objects/items/toys/board_games.dm
+++ code/game/objects/items/toys/board_games.dm
@@ -1,13 +1,16 @@
 /datum/chess_piece
 	var/name = "chess piece"
 	var/color = "white"
+	/// if 0, its not in a board
+	var/board_piece_id = 0
 	var/board_x_pos = 0
 	var/board_y_pos = 0
+	var/played = FALSE
 
-/datum/chess_piece/New(var/set_name, var/set_color)
+/datum/chess_piece/New(set_name, set_color, set_id)
 	color = "[set_color]"
 	name = "[set_name]"
-
+	board_piece_id = set_id
 /// Toy Board Parent Object, for board games
 /obj/item/toy/board
 	name = "Chess Board"
@@ -17,49 +20,38 @@
 	w_class = SIZE_MEDIUM
 	has_special_table_placement = TRUE
 
-	var/list/white_chess_pieces = list(
-		new /datum/chess_piece("rook", "white"),
-		new /datum/chess_piece("rook", "white"),
-		new /datum/chess_piece("knight", "white"),
-		new /datum/chess_piece("knight", "white"),
-		new /datum/chess_piece("bishop", "white"),
-		new /datum/chess_piece("bishop", "white"),
-		new /datum/chess_piece("queen", "white"),
-		new /datum/chess_piece("king", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white"),
-		new /datum/chess_piece("pawn", "white")
-	)
+	/// Determines what is to be moved
+	var/selected_piece;
+	/// Unused for now
+	var/max_pieces = 64
+	/// Chess pieces on the board
+	var/list/chess_pieces = list()
 
-	var/list/black_chess_pieces = list(
-		new /datum/chess_piece("rook", "black"),
-		new /datum/chess_piece("rook", "black"),
-		new /datum/chess_piece("knight", "black"),
-		new /datum/chess_piece("knight", "black"),
-		new /datum/chess_piece("bishop", "black"),
-		new /datum/chess_piece("bishop", "black"),
-		new /datum/chess_piece("queen", "black"),
-		new /datum/chess_piece("king", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black"),
-		new /datum/chess_piece("pawn", "black")
-	)
-
+	/// Testing, Remove once finished
 	var/TestValue = 0
 
 
 /obj/item/toy/board/Initialize()
 	. = ..()
+	generatepieces()
+
+/// Generate the Chess Pieces
+/obj/item/toy/board/proc/generatepieces()
+	/// Used to generate the ID
+	var/id_count = 1
+	for(var/color as anything in list("black", "white"))
+		// Pawns
+		for(var/count = 1 to 8)
+			chess_pieces += new /datum/chess_piece("pawn", color, id_count++)
+
+		// Rook/Knight/Bishop
+		for(var/piece_name as anything in list("rook", "knight", "bishop"))
+			chess_pieces += new /datum/chess_piece(piece_name, color, id_count++)
+			chess_pieces += new /datum/chess_piece(piece_name, color, id_count++)
+
+		// King/Queen
+		for(var/piece_name as anything in list("king", "queen"))
+			chess_pieces += new /datum/chess_piece(piece_name, color, id_count++)
 
 /// When Chess Board is moved, the board shakens
 /obj/item/toy/board/Move(NewLoc, direct)
@@ -95,21 +87,21 @@
 
 /obj/item/toy/board/ui_data(mob/user)
 	var/list/data = list()
-	data["white_chess_pieces"] = list()
-	data["black_chess_pieces"] = list()
+	data["selected"] = selected_piece
+	data["chess_pieces"] = list()
 
-	for(var/datum/chess_piece/piece as anything in white_chess_pieces)
+	// Send the chess pieces data that is on the board
+	for(var/datum/chess_piece/piece as anything in chess_pieces)
 		var/list/piece_data = list()
 		piece_data["name"] = piece.name
 		piece_data["color"] = piece.color
-		data["white_chess_pieces"] += list(piece_data)
+		piece_data["id"] = piece.board_piece_id
+		piece_data["played"] = piece.played
+		piece_data["xposition"] = piece.board_x_pos
+		piece_data["yposition"] = piece.board_y_pos
+		data["chess_pieces"] += list(piece_data)
 
-	for(var/datum/chess_piece/piece as anything in black_chess_pieces)
-		var/list/piece_data = list()
-		piece_data["name"] = piece.name
-		piece_data["color"] = piece.color
-		data["black_chess_pieces"] += list(piece_data)
-
+	// Delete After Testing
 	data["health"] = TestValue
 	data["color"] = "red"
 	return data
@@ -122,3 +114,29 @@
 	switch(action)
 		if("test")
 			TestValue += 1
+			. = TRUE
+		if("Select")
+			selected_piece = params
+
+			if(params["id"] == selected_piece["id"])
+				selected_piece = list()
+				to_chat(usr, SPAN_WARNING("You take the chess piece"))
+				// Insert Action to Grab Chess piece to hand
+
+			. = TRUE
+		if("Place")
+			// Position relative to the chess board
+			var/xposition = text2num(params["xposition"])
+			var/yposition = text2num(params["yposition"])
+
+			// No Chess Piece was selected
+			if(!selected_piece)
+				to_chat(usr, SPAN_WARNING("Select a Chess Piece to move"))
+				return . = TRUE
+			else
+				var/selected_id = selected_piece["id"]-1
+				chess_pieces[selected_id]["played"] = TRUE
+				chess_pieces[selected_id]["xposition"] = xposition
+				chess_pieces[selected_id]["yposition"] = yposition
+
+			. = TRUE
--- tgui/packages/tgui/interfaces/ChessBoard.js
+++ tgui/packages/tgui/interfaces/ChessBoard.js
@@ -11,32 +11,34 @@ export const ChessBoard = (props, context) => {
         <WhiteChessPieces />
         <ChessBoardContent />
         <BlackChessPieces />
-        <Testing />
       </Window.Content>
     </Window>
   );
 };
 
-const ChessPieceBackgroundColor = "gray";
-
 // Generate White Pieces
 const WhiteChessPieces = (props, context) => {
-
+  let color;
   const { act, data } = useBackend(context);
   const {
-    white_chess_pieces,
+    chess_pieces, selected,
   } = data;
+  // Filter White Pieces
+  const filtered_chess_pieces = chess_pieces.filter(piece => piece.color === "white");
+  // Filter Pieces in Play
+  const white_chess_pieces = filtered_chess_pieces.filter(piece => piece.played === 0);
   return (
     <Section title="White Chess Pieces">
       <Flex wrap="wrap" align="center" justify="space-evenly">
-        {white_chess_pieces.map((piece, index) => (
-          <Flex.Item key={index}>
+        {white_chess_pieces.map((piece) => (
+          <Flex.Item key={piece.id}>
             <Button
-            backgroundColor={ChessPieceBackgroundColor}
+            backgroundColor={selected[0][0].id === piece.id ? "green" : "gray"}
             textColor="white"
-            m={"2px"}
+            m="2px"
             content={piece.color + " " + piece.name}
-            icon={"fa-solid fa-chess-" + piece.name} />
+            icon={"fa-solid fa-chess-" + piece.name}
+            onClick={() => act("Select", { piece })} />
           </Flex.Item>
         ))}
       </Flex>
@@ -49,21 +51,24 @@ const BlackChessPieces = (props, context) => {
 
   const { act, data } = useBackend(context);
   const {
-    black_chess_pieces,
+    chess_pieces, selected,
   } = data;
+  // Filter Black Pieces
+  const filtered_chess_pieces = chess_pieces.filter(piece => piece.color === "black");
+  // Filter Pieces in Play
+  const black_chess_pieces = filtered_chess_pieces.filter(piece => piece.played === 0);
   return (
     <Section title="Black Chess Pieces">
       <Flex wrap="wrap" align="center" justify="space-evenly">
         {black_chess_pieces.map((piece, index) => (
           <Flex.Item key={index}>
             <Button
-            textAlign={"center"}
-            verticalAlignContent={"middle"}
-            backgroundColor={ChessPieceBackgroundColor}
+            backgroundColor={selected[0][0].id === piece.id ? "green" : "gray"}
             textColor="black"
-            m={"2px"}
+            m="2px"
             content={piece.color + " " + piece.name}
-            icon={"fa-solid fa-chess-" + piece.name} />
+            icon={"fa-solid fa-chess-" + piece.name}
+            onClick={() => act("Select", { piece })} />
           </Flex.Item>
         ))}
       </Flex>
@@ -71,79 +76,51 @@ const BlackChessPieces = (props, context) => {
   );
 };
 
-const GenerateRows = () => {
+const GenerateRows = (props, context) => {
+  const { act, data } = useBackend(context);
   const rows = [];
   for(let x = 0; x < 8; x++) {
     const row = [];
     const order = x % 2 === 1 ? 0 : 1;
     for(let y = 0; y < 8; y++) {
-      row.push(GenerateCell(y + order));
+      row.push(GenerateCell(data, act, y + order, x+1, y+1));
     }
     rows.push(<Table.Row>{row}</Table.Row>);
   }
   return <Table> { rows } </Table>;
 };
 
-const GenerateCell = (index) => {
+const GenerateCell = (data, act, index, xpos, ypos) => {
+  // for generating the mirrored tile
   const color = index % 2 === 1 ? "#564424" : "#ad9264";
+  // for checking if the tile has a chess piece on it
+  const chess_piece_in_play = data.chess_pieces.filter(piece => piece.played === 1);
+  const chess_piece_to_gen = chess_piece_in_play.find(piece => piece.xposition === xpos && piece.yposition === ypos);
   return (
   <Table.Cell
   backgroundColor={color}
-  height={"8vh"}
-  width={"8vw"}>
-    <Button fluid
-    backgroundColor={"transparent"} // Red for Testing and Transparent on Use
-    height={"100%"}
-    width={"100%"}
-    textColor="rgba(0, 0, 0, 0)"
-    color="transparent"
-    onClick={() => act('test')} />
-  </Table.Cell>);
+  height="8vh"
+  width="8vw"
+  textColor={chess_piece_to_gen.color} // Delete After
+  icon={"fa-solid fa-chess-" + chess_piece_to_gen.name}
+  onClick={() => act("Place", { xposition : xpos, yposition : ypos })} >
+    {xpos + " " + ypos + " remove later"}
+  </Table.Cell>
+  );
 };
 
 const ChessBoardContent = (props, context) => {
   const { act, data } = useBackend(context);
   const {
     health,
-    color,
   } = data;
   // Front End
   return (
     <Section title="Chess Board">
-      {GenerateRows()}
+      <GenerateRows />
+      <Button
+          content={health}
+          backgroundColor="red" />
     </Section>
-  );
-};
-
-const Testing = () => {
-  return (
-    <Table>
-      <Table.Row>
-        <Table.Cell>
-          <Button fluid preserveWhitespace
-          m={"0%"}
-          content={"TESTING"}
-          backgroundColor={"red"} />
-        </Table.Cell>
-        <Table.Cell>
-          <Button fluid preserveWhitespace
-          m={"0%"}
-          content={"TESTING"}
-          backgroundColor={"red"} />
-        </Table.Cell>
-        <Table.Cell>
-          <Button fluid preserveWhitespace
-          m={"0%"}
-          content={"TESTING"}
-          backgroundColor={"red"} />
-        </Table.Cell>
-        <Table.Cell>
-          <Button fluid preserveWhitespace
-          m={"0%"}
-          content={"TESTING"}
-          backgroundColor={"red"} />
-        </Table.Cell>
-      </Table.Row>
-    </Table>
   );
 };
